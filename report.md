da369f866c73f4428f0656ebf5341f269cf363c9
Connie Liaw Hong Yao <connieliaw1108@1utar.edu.my>
2016-03-11 01:11:16 +0800

    Added task1.py

--- report.md
+++ report.md
@@ -1,8 +1,8 @@
 UECM3033 Assignment #2 Report
 ========================================================
 
-- Prepared by: ** Put your name here**
-- Tutorial Group: T2/T3
+- Prepared by: Connie Liaw Hong Yao
+- Tutorial Group: T3
 
 --------------------------------------------------------
 
@@ -10,28 +10,56 @@ UECM3033 Assignment #2 Report
 
 The reports, codes and supporting documents are to be uploaded to Github at: 
 
-[https://github.com/your_github_id/UECM3033_assign1](https://github.com/your_github_id/UECM3033_assign1)
+[https://github.com/AlferdFoo/UECM3033_assign2](https://github.com/AlferdFoo/UECM3033_assign2)
 
 Explain your selection criteria here.
 
+In order to choose SOR method, the matrix A must be a positive definite and tridiagonal matrix. If it is not, then LU factorization is the only choice.
+
 Explain how you implement your `task1.py` here.
+
+First, define LU(A,b) and sor(A,b).
+Then, give the condition on solve(A,b).
+Final, run the system to the step solve(A,b) to choose which method is suitble.
+
+Solution:
+
+Solve by lu(A,b)
+[1.0, 1.0, 1.0]
+Solve by lu(A,b)
+[1.0000000000000067, -0.9999999999999961, 4.000000000000001,
+ -3.4999999999999956, 7.0000000000000036, -1.0000000000000004]
 
 ---------------------------------------------------------
 
 ## Task 2 -- SVD method and image compression
 
-Put here your picture file (Lenna.png)
+Put here your picture file (beautiful.png)
 
-![Lenna.png](Lenna.png)
+![cute.png](cute.png)
 
 How many non zero element in $\Sigma$?
+
+800 in r$\Sigma$; 800 in g$\Sigma$; 800 in b$\Sigma$
 
 Put here your lower and better resolution pictures. Explain how you generate
 these pictures from `task2.py`.
 
+![badCute.png](badCute.png)
+
+Change the $\Sigma$, which is matrix s of r, g and b where keep the first 30 non-zero elements and others become 0.
+Then multiply with matrices U and V of r, g and b to create newR, newG and newB.
+At last, combine the 3 different matrices as a newImage.
+
+![goodCute.png](goodCute.png)
+
+Similarly, change the $\Sigma$, which is matrix s of r, g and b but keep the first 200 non-zero elements and others become 0.
+Then multiply with matrices U and V of r, g and b to create newR1, newG1 and newB1.
+At last, combine the 3 different matrices as a newImage1.
+
 What is a sparse matrix?
 
-
+A sparse matrix is a matrix in which most of the elements are zero.
 -----------------------------------
 
-<sup>last modified: change your date here</sup>
+<sup>last modified: 11/3/2016</sup>
--- task2.py
+++ task2.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-img=mpimg.imread('Lenna.tiff')
+img = mpimg.imread('cute.png')
 [r,g,b] = [img[:,:,i] for i in range(3)]
-
 
 fig = plt.figure(1)
 ax1 = fig.add_subplot(2,2,1)
@@ -17,3 +16,95 @@ ax3.imshow(g, cmap = 'Greens')
 ax4.imshow(b, cmap = 'Blues')
 plt.show()
 
+#extract r, g and b into USV
+rU, rS, rV = np.linalg.svd(r, full_matrices = False)
+gU, gS, gV = np.linalg.svd(g, full_matrices = False)
+bU, bS, bV = np.linalg.svd(b, full_matrices = False)
+
+#calculate non-zero element in matrix S of r, g and b
+red = np.count_nonzero(rS)
+green = np.count_nonzero(gS)
+blue = np.count_nonzero(bS)
+
+#display the number of non-zero element of r, g and b in matrix S
+print("Total number of non-zero elements in S of r:", red)
+print("Total number of non-zero elements in S of g:", green)
+print("Total number of non-zero elements in S of b:", blue)
+
+#create 3 different matrices
+rs1 = [0 for x in range (0,red)]
+gs1 = [0 for x in range (0,green)]
+bs1 = [0 for x in range (0,blue)]
+
+#change the matrix S of r, g and b
+#where only keep the first 30 non-zero elements
+for i in range (0, red):
+    if (i < 30):
+        rs1[i] = rS[i]
+    else:
+        rs1[i] = 0
+       
+for i in range (0, green):
+    if (i < 30):
+        gs1[i] = gS[i]
+    else:
+        gs1[i] = 0
+
+for i in range (0, blue):
+    if (i < 30):
+        bs1[i] = bS[i]
+    else:
+        bs1[i] = 0
+
+#convert matrix s1 into diagonal matrix S1 of r, g and b
+rS1 = np.diag(rs1)
+gS1 = np.diag(gs1)
+bS1 = np.diag(bs1)
+
+#create new matrices by multiply matrix U, S1 and V of r, g, b
+newR = np.dot(rU, np.dot(rS1,rV))
+newG = np.dot(gU, np.dot(gS1,gV))
+newB = np.dot(bU, np.dot(bS1,bV))
+
+#create image from new matrices, save and display
+badCute = np.dstack([newR, newG, newB])
+plt.imsave('badCute.png', badCute)
+plt.imshow(badCute)
+plt.show()
+
+#below same with the scenario above
+#but just keep the first 200 non-zero elements instead of 30
+rs2 = [0 for x in range (0,red)]
+gs2 = [0 for x in range (0,green)]
+bs2 = [0 for x in range (0,blue)]
+
+for i in range (0, red):
+    if (i < 200):
+        rs2[i] = rS[i]
+    else:
+        rs2[i] = 0
+        
+for i in range (0, green):
+    if (i < 200):
+        gs2[i] = gS[i]
+    else:
+        gs2[i] = 0
+
+for i in range (0, blue):
+    if (i < 200):
+        bs2[i] = bS[i]
+    else:
+        bs2[i] = 0        
+
+rS2 = np.diag(rs2)
+gS2 = np.diag(gs2)
+bS2 = np.diag(bs2)
+
+newR1 = np.dot(rU, np.dot(rS2,rV))
+newG1 = np.dot(gU, np.dot(gS2,gV))
+newB1 = np.dot(bU, np.dot(bS2,bV))
+
+goodCute = np.dstack([newR1, newG1, newB1])
+plt.imsave('goodCute', goodCute)
+plt.imshow(goodCute)
+plt.show()
